d21c0a939f34eb0662c3f6b24d0cb1beace89d7d
Anton <poliskoanton@gmail.com>
2024-12-09 12:12:09 -0700

    Add files via upload

---
+++ analyzerBot-main/telegramAnalyzingProject/analyzer-api/src/main/java/com/example/restservice/Entity/Message.java
@@ -0,0 +1,36 @@
+package com.example.restservice.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    private final String content;
+    @NotNull
+    private final int date;
+
+
+    public Message(String content, int date) {
+        this.content = content;
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public int getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + "Content:" + content + "\n" + "Date:" + date + "\n";
+    }
+}
